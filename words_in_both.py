7ac2330f0f2c318a57d2a122c1f77fca47c2ad5f
sophiacphilips <114261752+sophiacphilips@users.noreply.github.com>
2022-11-16 21:21:38 -0800

    second commit added set to result with "{}"

--- words_in_both.py
+++ words_in_both.py
@@ -11,6 +11,6 @@ def words_in_both(sent_1, sent_2):
     for word in sent_1:#iterating using word
         if word in sent_2 and word not in common_words: #if the word appears in both sentences it will list it, but it won't list it twice, if it appears more than once
             common_words.append(word) #adding string of common words to result
-    return common_words
+    return set(common_words)
 
-#print(words_in_both("She is a jack of all trades", "Jack was tallest of all"))
+print(words_in_both("She is a jack of all trades", "Jack was tallest of all"))
